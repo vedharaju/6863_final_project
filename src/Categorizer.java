5f92d0bbc6d37124d412c497ea45c514bd61fb3c
Vedha Sayyaparaju <vedha@leaf.me>
2012-12-08 16:45:03 -0500

    added files with positive and negative words.

---
+++ src/Categorizer.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+
+public class Categorizer {
+	
+	
+	public static void main(String[] args) throws ClassNotFoundException,
+	IOException {
+		System.out.println(RunMedPost.getWords("Hiii how are you so pretty?"));
+	}
+}
--- src/RunMedPost.java
+++ src/RunMedPost.java
@@ -1,8 +1,7 @@
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,7 +18,7 @@ public class RunMedPost {
 	static TokenizerFactory TOKENIZER_FACTORY = new RegExTokenizerFactory(
 			"(-|'|\\d|\\p{L})+|\\S");
 
-	public static void main(String[] args) throws ClassNotFoundException,
+	public static ArrayList<String> getWords(String line) throws ClassNotFoundException,
 			IOException {
 		String filename = "./pos-en-general-brown.HiddenMarkovModel";
 		System.out.println("Reading model from file=" + filename);
@@ -29,33 +28,34 @@ public class RunMedPost {
 		Streams.closeQuietly(objIn);
 		HmmDecoder decoder = new HmmDecoder(hmm);
 
-		InputStreamReader isReader = new InputStreamReader(System.in);
-		BufferedReader bufReader = new BufferedReader(isReader);
-		while (true) {
-			System.out.print("\n\nINPUT> ");
-			System.out.flush();
-			String line = bufReader.readLine();
-			if (line == null || line.length() < 1
-					|| line.equalsIgnoreCase("quit")
-					|| line.equalsIgnoreCase("exit"))
-				break;
-			char[] cs = line.toCharArray();
+		char[] cs = line.toCharArray();
+		Tokenizer tokenizer = TOKENIZER_FACTORY.tokenizer(cs, 0, cs.length);
+		String[] tokens = tokenizer.tokenize();
+		List<String> tokenList = Arrays.asList(tokens);
 
-			Tokenizer tokenizer = TOKENIZER_FACTORY.tokenizer(cs, 0, cs.length);
-			String[] tokens = tokenizer.tokenize();
-			List<String> tokenList = Arrays.asList(tokens);
-
-			firstBest(tokenList, decoder);
-		}
-		Streams.closeQuietly(bufReader);
+//		firstBest(tokenList, decoder);
+		return filteredFirstBest(tokenList, decoder);
 	}
 
-	static void firstBest(List<String> tokenList, HmmDecoder decoder) {
+	static ArrayList<String> firstBest(List<String> tokenList, HmmDecoder decoder) {
+		ArrayList<String> words_with_tags = new ArrayList<String>();
 		Tagging<String> tagging = decoder.tag(tokenList);
-		System.out.println("\nFIRST BEST");
 		for (int i = 0; i < tagging.size(); ++i)
-			System.out.print(tagging.token(i) + "_" + tagging.tag(i) + " ");
-		System.out.println();
+			words_with_tags.add(tagging.token(i) + "_" + tagging.tag(i));
+		return words_with_tags;
+	}
 
+	static ArrayList<String> filteredFirstBest(List<String> tokenList, HmmDecoder decoder) {
+		String[] tags = { "ql", "qlp", "jj", "jj$", "jj+jj", "jjr", "jjr+cs",
+				"jjs", "jjt", "rb", "rb$", "rb+bez", "rb+cs", "rbr", "rbr+cs",
+				"rbt", "rn", "rp", "rp+in" };
+		List<String> acceptable_tags = Arrays.asList(tags);
+		ArrayList<String> filtered_words = new ArrayList<String>();
+		Tagging<String> tagging = decoder.tag(tokenList);
+		for (int i = 0; i < tagging.size(); ++i)
+			if (acceptable_tags.contains(tagging.tag(i))) {
+				filtered_words.add(tagging.token(i));
+			}
+		return filtered_words;
 	}
 }
